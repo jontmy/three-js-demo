7a4f3f76c3a95b71f098d42aa5fff8a48aa1313e
Jonathan Tay <jonathantaymy@gmail.com>
2023-11-25 21:41:16 +0800

    feat: add more cubes and a light source

--- src/01-cube.ts
+++ src/01-cube.ts
@@ -3,21 +3,44 @@ import "./style.css";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 7;
 
+// Cube
+function createCube(geometry: THREE.BoxGeometry, color: number, y: number) {
+    const material = new THREE.MeshPhongMaterial({ color });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.y = y;
+    scene.add(cube);
+    return cube;
+}
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xa9dc76 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 5;
+const cubes = [
+    createCube(geometry, 0xff6188, 2),
+    createCube(geometry, 0xa9dc76, 0),
+    createCube(geometry, 0x78dce8, -2),
+];
+
+// Light
+const color = 0xffffff;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+function render(time: number) {
+    time /= 1000;
+
+    cubes.forEach((cube, i) => {
+        const speed = 1 + i * 0.1;
+        const rot = time * speed;
+        cube.rotation.x = rot;
+        cube.rotation.y = rot;;
+    })
     renderer.render(scene, camera);
+    requestAnimationFrame(render);
 }
-animate();
+requestAnimationFrame(render);
